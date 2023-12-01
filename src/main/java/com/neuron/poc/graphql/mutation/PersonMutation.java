cac041d20178ecd2904ecc6263c2ce4a35fb7808
venkata <munnalobo@yahoo.com>
2023-12-01 14:50:48 -0500

    - Making Person mutation.

--- src/main/java/com/neuron/poc/graphql/mutation/PersonMutation.java
+++ src/main/java/com/neuron/poc/graphql/mutation/PersonMutation.java
@@ -8,18 +8,22 @@ import graphql.kickstart.annotations.GraphQLMutationResolver;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Service
 @NoArgsConstructor
 @GraphQLMutationResolver
+@RestController
 @Slf4j
 public class PersonMutation {
-
 
   @GraphQLField
   @GraphQLNonNull
   @GraphQLDescription("Creates a new person.")
-  public static Person createPerson(final @GraphQLNonNull Person createPerson) {
+  @PostMapping("/people/create")
+  public static Person createPerson(final @RequestBody @GraphQLNonNull Person createPerson) {
     log.info("Create person input: {}", createPerson);
     final Person newPerson =
         Person.builder()
